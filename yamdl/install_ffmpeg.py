3f93e1829702e94b3469a4bf8a198e873a8fca5b
Aditya <37655818+adi-075@users.noreply.github.com>
2024-08-03 21:58:43 -0400

    Add support for Linux Distributions

--- yamdl/install_ffmpeg.py
+++ yamdl/install_ffmpeg.py
@@ -15,12 +15,31 @@ def install_ffmpeg():
             sys.exit(1)
 
     elif system == 'linux':
-        subprocess.run(['sudo', 'apt', 'update'], check=True)
-        subprocess.run(['sudo', 'apt', 'install', '-y', 'ffmpeg'], check=True)
-        print('ffmpeg has been installed.')
+        distro = None
+        try:
+            with open('/etc/os-release') as f:
+                for line in f:
+                    if line.startswith('ID='):
+                        distro = line.strip().split('=')[1]
+                        break
+        except FileNotFoundError:
+            print('Unable to determine Linux distribution.')
+            sys.exit(1)
+
+        if distro in ['ubuntu', 'debian']:
+            subprocess.run(['sudo', 'apt', 'update'], check=True)
+            subprocess.run(['sudo', 'apt', 'install', '-y', 'ffmpeg'], check=True)
+            print('ffmpeg has been installed on Debian-based system.')
+        elif distro in ['arch', 'manjaro']:
+            subprocess.run(['sudo', 'pacman', '-Syu', '--noconfirm'], check=True)
+            subprocess.run(['sudo', 'pacman', '-S', '--noconfirm', 'ffmpeg'], check=True)
+            print('ffmpeg has been installed on Arch-based system.')
+        else:
+            print(f'Unsupported Linux distribution: {distro}')
+            sys.exit(1)
     elif system == 'darwin':  # macOS
         subprocess.run(['brew', 'install', 'ffmpeg'], check=True)
-        print('ffmpeg has been installed.')
+        print('ffmpeg has been installed on macOS.')
     else:
         print(f'Unsupported OS: {system}')
         sys.exit(1)
